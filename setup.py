514460852f3c857216fd985b01bad1764a960acb
Wei-Chen-hub <1259566226@qq.com>
2024-03-12 15:35:54 +0800

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="hdtools",
+    version="0.1",
+    author="Wei Chen",
+    author_email="wc1259566226@gmail.com",
+    description="Tools for processing HumanData",
+    packages=['utils', 'mapping', 'converter'],
+)
